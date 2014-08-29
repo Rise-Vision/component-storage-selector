0366f7cee18768be2729d9709737c0f9fa634b58
simon-ramsay <simon@rangle.io>
2014-08-29 13:29:23 -0400

    created angular wrapper for loading the storage client

---
+++ dist/component-storage-loader.js
@@ -0,0 +1,51 @@
+(function (angular) {
+
+  'use strict';
+
+  angular
+  .module('risevision.widget.common.storage-client-loader', [])
+  .directive('storageClientLoader', ['$window','$templateCache',
+    function($window, $templateCache){
+      return {
+        restrict: 'E',
+        scope : {
+           companyid : '='
+        },
+        template: $templateCache.get('loader.html'),
+        link: function (scope) {
+          scope.show = false;
+          scope.storageUrl = 'http://storage.risevision.com/storage-modal.html#/files/'+scope.companyId;
+
+          $window.addEventListener('message',
+            function (event) {
+              if (event.origin !== 'http://storage.risevision.com') { return; }
+              console.log(event.data);
+            }, false);
+
+          scope.showSelector = function(){
+            scope.show = true;
+          };
+          scope.hideSelector = function(){
+            scope.show = false;
+          };
+        }//link
+      };//return
+   }//function
+  ]);//directive
+})(angular);
+
+
+
+(function(module) {
+try { app = angular.module("risevision.widget.common.storage-client-loader"); }
+catch(err) { app = angular.module("risevision.widget.common.storage-client-loader", []); }
+app.run(["$templateCache", function($templateCache) {
+  "use strict";
+  $templateCache.put("loader.html",
+    "<button class=\"btn btn-widget-icon-storage\" ng-click=\"showSelector()\" type=\"button\" />\n" +
+    "<div ng-if=\"show\">\n" +
+    "   <div class=\"storage-selector-backdrop\" ng-click=\"hideSelector()\"></div>\n" +
+    "  <iframe class=\"storage-selector-iframe\" src=\"{{storageUrl}}\"></iframe>\n" +
+    "</div>");
+}]);
+})();
