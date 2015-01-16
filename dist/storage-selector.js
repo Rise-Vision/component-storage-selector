88a7c6c78e4a673104b64bd29af42801ef0b78cc
Stuart Lees <stuartlees@gmail.com>
2015-01-16 09:48:03 -0500

    Remove unnecessary CONFIG var
    - Updated all config files to remove the CONFIG var creation as this component is Angular
    - Setting the value of `STORAGE_MODAL` in all config files to the production version as it's the value that will be needed when built

--- dist/storage-selector.js
+++ dist/storage-selector.js
@@ -1,14 +1,6 @@
-/* global CONFIG: true */
-/* exported CONFIG */
-if (typeof CONFIG === "undefined") {
-  var CONFIG = {
-    // variables go here
-  };
-
-  if (typeof angular !== "undefined") {
-    angular.module("risevision.widget.common.storage-selector.config", [])
-      .constant("STORAGE_MODAL", "http://storage.risevision.com/~rvi/storage-client-rva-test/storage-modal.html#/files/");
-  }
+if (typeof angular !== "undefined") {
+  angular.module("risevision.widget.common.storage-selector.config", [])
+    .constant("STORAGE_MODAL", "http://storage.risevision.com/storage-modal.html#/files/");
 }
 
 (function () {
--- src/config/prod.js
+++ src/config/prod.js
@@ -1,12 +1,4 @@
-/* global CONFIG: true */
-/* exported CONFIG */
-if (typeof CONFIG === "undefined") {
-  var CONFIG = {
-    // variables go here
-  };
-
-  if (typeof angular !== "undefined") {
-    angular.module("risevision.widget.common.storage-selector.config", [])
-      .constant("STORAGE_MODAL", "http://storage.risevision.com/storage-modal.html#/files/");
-  }
+if (typeof angular !== "undefined") {
+  angular.module("risevision.widget.common.storage-selector.config", [])
+    .constant("STORAGE_MODAL", "http://storage.risevision.com/storage-modal.html#/files/");
 }
