3f61be8a55b1748426f16b7e6fe24d083ef1ab4d
Stuart Lees <stuartlees@gmail.com>
2015-01-16 17:25:14 -0500

    Changing constant to value in config
    - Need to change from `constant` to `value` so the `STORAGE_MODAL` value can be overridden (in a widget)

--- dist/storage-selector.js
+++ dist/storage-selector.js
@@ -1,6 +1,6 @@
 if (typeof angular !== "undefined") {
   angular.module("risevision.widget.common.storage-selector.config", [])
-    .constant("STORAGE_MODAL", "http://storage.risevision.com/storage-modal.html#/files/");
+    .value("STORAGE_MODAL", "http://storage.risevision.com/storage-modal.html#/files/");
 }
 
 (function () {
